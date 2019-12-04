eca943bfc84fa60d64a33671f3e12247fa80371c
Risingdeveloper007 <rising.hunter@yandex.com>
2019-12-04 23:16:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,190 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+
+import "./App.css";
+import logo from "./images/title_logo.png";
+import AppGroup from "./components/app_group.js";
+import data from "./data";
+const {
+    first_app_group,
+    second_app_group,
+    third_app_group,
+    fourth_app_group,
+    fifth_app_group,
+} = data;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Fragment>
+            <div
+                data-collapse="none"
+                data-animation="default"
+                data-duration="400"
+                className="logo-wrapper w-nav"
+            >
+                <div className="w-nav-overlay" data-wf-ignore=""></div>
+            </div>
+            <section className="main-wrapper">
+                <main className="container top-container">
+                    <a href="/submit" className="link-block w-inline-block">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoNCpWvNxgSDx7Yepg4_v8znTOet8a0OFzFYrPsRzrVBDugKVj&s"
+                            width="31"
+                            alt=""
+                        />
+                    </a>
+                    <h1 className="link-block w-inline-block">
+                        <img src={logo} height="60" alt="" />
+                    </h1>
+                    <a href="/about" className="link-block w-inline-block">
+                        <img
+                            src="https://www.netclipart.com/pp/m/24-248220_file-zhwp-question-mark-svg-wikimedia-commons-google.png"
+                            width="31"
+                            alt=""
+                        />
+                    </a>
+                </main>
+            </section>
+            <AppGroup {...first_app_group} />
+            <AppGroup {...second_app_group} />
+            <section className="wrapper">
+                <div className="main-container main-containter-transparent">
+                    <div className="columns w-row">
+                        <div className="column-1 w-col w-col-4">
+                            <a
+                                href="https://techwiser.com/airpods-apps-for-android/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                className="blog-item w-inline-block"
+                            >
+                                <div className="blog-header-img"></div>
+                                <div className="div-block-4">
+                                    <h1 className="category-heading">How To</h1>
+                                    <h1 className="heading-4">Experience Airpods On Android</h1>
+                                    <p className="descrip">
+                                        Best Airpods Apps for Android to Get iPhone Like Experience
+                                        <br />
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="column-1 w-col w-col-4">
+                            <a
+                                href="https://www.macrumors.com/guide/airpods-pro-tips-and-tricks/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                className="blog-item w-inline-block"
+                            >
+                                <div className="blog-header-img airpods-pro-image"></div>
+                                <div className="div-block-4">
+                                    <h1 className="category-heading">Tips &amp;&nbsp;Tricks</h1>
+                                    <h1 className="heading-4">
+                                        Top 14 AirPods Pro Tips and Tricks
+                                    </h1>
+                                    <p className="descrip">
+                                        Best Airpods Apps for Android to Get iPhone Like Experience
+                                        <br />
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="column-1 column2 w-col w-col-4">
+                            <a
+                                href="https://techwiser.com/airpods-apps-for-android/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                className="blog-item w-inline-block"
+                            >
+                                <div className="blog-header-img"></div>
+                                <div className="div-block-4">
+                                    <h1 className="category-heading">How To</h1>
+                                    <h1 className="heading-4">Experience Airpods On Android</h1>
+                                    <p className="descrip">
+                                        Best Airpods Apps for Android to Get iPhone Like Experience
+                                        <br />
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <AppGroup {...third_app_group} />
+            <AppGroup {...fourth_app_group} />
+            <section className="wrapper">
+                <div className="main-container main-containter-transparent">
+                    <div className="columns w-row">
+                        <div className="column-1 w-col w-col-6">
+                            <a
+                                href="https://techwiser.com/airpods-apps-for-android/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                className="blog-item w-inline-block"
+                            >
+                                <div className="blog-header-img"></div>
+                                <div className="div-block-4">
+                                    <h1 className="category-heading">How To</h1>
+                                    <h1 className="heading-4">Experience Airpods On Android</h1>
+                                    <p className="descrip">
+                                        Best Airpods Apps for Android to Get iPhone Like Experience
+                                        <br />
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="column-1 column2 w-col w-col-6">
+                            <a
+                                href="https://www.macrumors.com/guide/airpods-pro-tips-and-tricks/"
+                                rel="noopener noreferrer"
+                                target="_blank"
+                                className="blog-item w-inline-block"
+                            >
+                                <div className="blog-header-img airpods-pro-image"></div>
+                                <div className="div-block-4">
+                                    <h1 className="category-heading">Tips &amp;&nbsp;Tricks</h1>
+                                    <h1 className="heading-4">
+                                        Top 14 AirPods Pro Tips and Tricks
+                                    </h1>
+                                    <p className="descrip">
+                                        Best Airpods Apps for Android to Get iPhone Like Experience
+                                        <br />
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <AppGroup {...fifth_app_group} />
 
+            <footer className="wrapper">
+                <div className="container-copy">
+                    <a
+                        href="https://twitter.com/huntermoonshot"
+                        className="twitter-link w-inline-block"
+                    >
+                        <img
+                            src="https://uploads-ssl.webflow.com/5dbe53da15cb7fb363a3264c/5dcb89ad90ed86c5db33d654_iconmonstr-twitter-1.png"
+                            width="37"
+                            alt=""
+                            className="image-5"
+                        />
+                    </a>
+                    <a
+                        href="https://www.producthunt.com/posts/airpods-app-store"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="w-inline-block"
+                    >
+                        <img
+                            src="https://uploads-ssl.webflow.com/5dbe53da15cb7fb363a3264c/5ddc328858529f6521152296_Screen%20Shot%202019-11-25%20at%202.58.29%20PM.png"
+                            width="299"
+                            alt=""
+                            className="image-40"
+                        />
+                    </a>
+                </div>
+            </footer>
+        </Fragment>
+    );
+}
 export default App;
---
+++ src/components/app_group.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import get_icon from "../images/get_icon.png";
+class AppGroup extends Component {
+    render() {
+        const { category_heading, heading2, item_list } = this.props;
+        return (
+            <Router>
+                <section className="wrapper">
+                    <main className="main-container">
+                        <h1 className="category-heading">{category_heading}</h1>
+                        <h1 className="heading-2">{heading2}</h1>
+                        <div className="w-dyn-list">
+                            <div className="collection-list-2 w-dyn-items">
+                                {item_list.map((item, i) => (
+                                    <div
+                                        key={i}
+                                        className="collection-item-2 w-dyn-item"
+                                    >
+                                        <Link
+                                            to={`${item.link}`}
+                                            className="link-item w-inline-block"
+                                        >
+                                            <div className="icon-wrapper">
+                                                <div
+                                                    style={{
+                                                        backgroundImage: `url('${item.image}')`,
+                                                    }}
+                                                    className="icon"
+                                                ></div>
+                                            </div>
+
+                                            <div className="app-info">
+                                                <h1 className="app-name">
+                                                    {item.app_name}
+                                                </h1>
+                                                <h2 className="app-descrip">
+                                                    {item.app_description}
+                                                </h2>
+                                            </div>
+                                            <div className="get-wrapper">
+                                                <img
+                                                    src={get_icon}
+                                                    width="40"
+                                                    alt="Get"
+                                                />
+                                            </div>
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </main>
+                </section>
+            </Router>
+        );
+    }
+}
+
+export default AppGroup;
